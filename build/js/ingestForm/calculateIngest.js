ff9677e2d1c5c0efd80fe95488335da77f3e55d4
IrvRodFlo9JU <irvrodflo9@gmail.com>
2023-10-26 00:57:32 -0600

    function calculate ingest total

---
+++ build/js/ingestForm/calculateIngest.js
@@ -0,0 +1,2 @@
+const calculateIngest=()=>{const e=document.querySelectorAll(".water-equivalent");let t=0;return e.forEach(e=>{const l=e.querySelector(".equivalent-value").textContent;t+=parseInt(l)}),t};export default calculateIngest;
+//# sourceMappingURL=calculateIngest.js.map
---
+++ build/js/waterForm/calculateWater.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=calculateWater.js.map
